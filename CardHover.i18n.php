d6acb7fcd305379f1a398da02e6204a462b48e1b
Richard Stanway <r1ch@teamliquid.net>
2016-09-21 02:21:25 +0900

    Import CardHover to git

---
+++ CardHover.i18n.php
@@ -0,0 +1,15 @@
+<?php 
+
+$messages = array();
+
+$messages['en'] = array(
+	'cardhover-desc' => 'This extension generates hover images for internal links to card pages.',
+);
+
+$messages['qqq'] = array(
+	'cardhover-desc' => 'This extension generates hover images for internal links to card pages.',
+);
+
+
+
+?>
