7e8c5b1eb6e202efb6c3815823509a9f1a982dcc
FO-nTTaX <fo.nttax@yahoo.de>
2018-09-24 15:10:21 +0200

    code style

--- CardHover.js
+++ CardHover.js
@@ -1,23 +1,23 @@
 /* Card Hover */
-$(document).ready(function () {
-	$('.hovercard').hover(function() {
-		var imagelink = $(this).attr('data-img');
-		if (imagelink) {
-			imagelink = imagelink.replace(' ', '_');
-			var left = $(this).position().left + 20;
-			var top = $(this).position().top + 20;
-			if (($(window).width() - (left + 480)) < 0) {
+$( document ).ready( function() {
+	$( '.hovercard' ).hover( function() {
+		var imagelink = $( this ).attr( 'data-img' );
+		if ( imagelink ) {
+			imagelink = imagelink.replace( ' ', '_' );
+			var left = $( this ).position().left + 20;
+			var top = $( this ).position().top + 20;
+			if ( ( $( window ).width() - ( left + 480 ) ) < 0 ) {
 				left = left - 280;
 			}
-			if ((($(this).offset().top + 20) - $(window).scrollTop()) > ($(window).height()/2)) {
+			if ( ( ( $( this ).offset().top + 20 ) - $( window ).scrollTop() ) > ( $( window ).height() / 2 ) ) {
 				top = top - 416;
 			}
-			var imagediv = $('<div id="hoverimage"><img src="' + imagelink + '"></div>');
-			$(this).append(imagediv);
- 			imagediv.css('top', top);
-			imagediv.css('left', left);
+			var imagediv = $( '<div id="hoverimage"><img src="' + imagelink + '"></div>' );
+			$( this ).append( imagediv );
+			imagediv.css( 'top', top );
+			imagediv.css( 'left', left );
 		}
-	},function () {
-		$('#hoverimage').remove();
-	});
-});
+	}, function() {
+		$( '#hoverimage' ).remove();
+	} );
+} );
--- CardHover.php
+++ CardHover.php
@@ -1,6 +1,6 @@
-<?php 
+<?php
 
-if (!defined('MEDIAWIKI')) {
+if ( !defined( 'MEDIAWIKI' ) ) {
 	die();
 }
 
@@ -12,33 +12,34 @@ class CardHover {
 		global $wgDBprefix;
 
 		// This Hook is called multiple times in the parsing process, ensure this function is only executed
-		if (self::$executed)
+		if ( self::$executed ) {
 			return $parser;
+		}
 		self::$executed = true;
 
-		$dbr = wfGetDB(DB_SLAVE);
+		$dbr = wfGetDB( DB_SLAVE );
 
 		// Put ID of mainspace here
 		$namespace_id = 0;
 
-		/*$sql = "(SELECT p1.page_title \n"
-		. "FROM redirect\n"
-		. "INNER JOIN page p1 ON rd_from = p1.page_id\n"
-		. "INNER JOIN page p2 ON rd_title = p2.page_title\n"
-		. "WHERE p2.page_title IN (\n"
-		. "SELECT page_title\n"
-		. "FROM page\n"
-		. "LEFT JOIN categorylinks ON cl_from = page_id\n"
-		. "WHERE cl_to = 'Cards'\n"
-		. "AND page_namespace = ".$namespace_id." ))\n"
-		. "UNION\n"
-		. "(SELECT page_title\n"
-		. "FROM page\n"
-		. "INNER JOIN categorylinks ON cl_from = page_id\n"
-		. "WHERE cl_to = 'Cards'\n"
-		. "AND page_namespace = ".$namespace_id." )";*/
-	
-		$sql = "(SELECT p1.page_title 
+		/* $sql = "(SELECT p1.page_title \n"
+		  . "FROM redirect\n"
+		  . "INNER JOIN page p1 ON rd_from = p1.page_id\n"
+		  . "INNER JOIN page p2 ON rd_title = p2.page_title\n"
+		  . "WHERE p2.page_title IN (\n"
+		  . "SELECT page_title\n"
+		  . "FROM page\n"
+		  . "LEFT JOIN categorylinks ON cl_from = page_id\n"
+		  . "WHERE cl_to = 'Cards'\n"
+		  . "AND page_namespace = ".$namespace_id." ))\n"
+		  . "UNION\n"
+		  . "(SELECT page_title\n"
+		  . "FROM page\n"
+		  . "INNER JOIN categorylinks ON cl_from = page_id\n"
+		  . "WHERE cl_to = 'Cards'\n"
+		  . "AND page_namespace = ".$namespace_id." )"; */
+
+		$sql = "(SELECT p1.page_title
 			FROM {$wgDBprefix}redirect
 			INNER JOIN {$wgDBprefix}page p1 ON rd_from = p1.page_id
 			INNER JOIN {$wgDBprefix}page p2 ON rd_title = p2.page_title
@@ -55,35 +56,35 @@ class CardHover {
 				WHERE cl_to = 'Cards'
 				AND page_namespace = \"$namespace_id\")";
 
-		$result = $dbr->query($sql);
+		$result = $dbr->query( $sql );
 
 		$cards = array();
-		foreach ($result as $r) {
+		foreach ( $result as $r ) {
 			// Save both "card name" and "card name|" to avoid regex
 			// Store as key instead of value to allow efficient lookup
-			$cards[str_replace('_', ' ', $r->page_title)] = true;
-			$cards[str_replace('_', ' ', $r->page_title) . '|'] = true;
+			$cards[ str_replace( '_', ' ', $r->page_title ) ] = true;
+			$cards[ str_replace( '_', ' ', $r->page_title ) . '|' ] = true;
 		}
 
 		// get all internal links
 		//preg_match_all('/\[\[(.*)\]\]/', $text, $internal_links);
-		preg_match_all('/\[\[(.*?)\]\]/', $text, $internal_links);
-		$matches = array_unique($internal_links[1]);
+		preg_match_all( '/\[\[(.*?)\]\]/', $text, $internal_links );
+		$matches = array_unique( $internal_links[ 1 ] );
 		// check if internal link links to card page
-		foreach ($matches as $match) {
+		foreach ( $matches as $match ) {
 			// Internal link: [[Link|Arbitrary Name]]
-			if (strpos($match,'|') !== false) {
-				$expl = explode('|', $match);
-				$cardmatch = $expl[0] . '|';
-				if (array_key_exists($cardmatch, $cards)) {
-					$replace = '<span class="hovercard" data-img="{{#show:'.$expl[0].'|?has filepath|link=none}}">[['.$match.']]</span>';
-					$text = str_replace('[['.$match.']]', $replace, $text);
+			if ( strpos( $match, '|' ) !== false ) {
+				$expl = explode( '|', $match );
+				$cardmatch = $expl[ 0 ] . '|';
+				if ( array_key_exists( $cardmatch, $cards ) ) {
+					$replace = '<span class="hovercard" data-img="{{#show:' . $expl[ 0 ] . '|?has filepath|link=none}}">[[' . $match . ']]</span>';
+					$text = str_replace( '[[' . $match . ']]', $replace, $text );
 				}
 
-			// Internal link: [[Link]]
-			} else if (array_key_exists($match, $cards)) {
-				$replace = '<span class="hovercard" data-img="{{#show:'.$match.'|?has filepath|link=none}}">[['.$match.']]</span>';
-				$text = str_replace('[['.$match.']]', $replace, $text);
+				// Internal link: [[Link]]
+			} else if ( array_key_exists( $match, $cards ) ) {
+				$replace = '<span class="hovercard" data-img="{{#show:' . $match . '|?has filepath|link=none}}">[[' . $match . ']]</span>';
+				$text = str_replace( '[[' . $match . ']]', $replace, $text );
 			}
 		}
 		return $parser;
@@ -93,6 +94,5 @@ class CardHover {
 		$out->addModules( 'ext.cardHover' );
 		return true;
 	}
-}
 
-?>
+}
