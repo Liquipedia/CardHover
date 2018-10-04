f1d9248ead5f411e5f631d71c7f7c471ccd4e7e6
FO-nTTaX <fo.nttax@yahoo.de>
2018-10-04 17:58:08 +0200

    rewrite without jQuery

--- CardHover.js
+++ CardHover.js
@@ -4,27 +4,30 @@
 		document.querySelectorAll( '.hovercard' ).forEach( function( card ) {
 			var link = card.dataset.img;
 			card.addEventListener( 'mouseover', function() {
-				if ( document.querySelector( '.hoverimage' ) !== null ) {
-					var position = card.getBoundingClientRect();
-					var top = position.top + 50;
-					var left = position.left + 20;
-					if ( position.top + 50 > ( window.innerHeight / 2 ) ) {
-						top = top - 446;
-					}
-					if ( ( window.innerWidth - ( left + 480 ) ) < 0 ) {
-						left = left - 280;
-					}
-					var img = document.createElement( 'img' );
-					img.src = link;
-					var div = document.createElement( 'div' );
-					div.classList.add( 'hoverimage' );
-					div.appendChild( img );
-					div.style.top = Math.round( top ).toString() + 'px';
-					div.style.left = Math.round( left ).toString() + 'px';
-					card.appendChild( div );
+				var position = card.getBoundingClientRect();
+				var top = position.top + 50;
+				var left = position.left + 20;
+				if ( position.top + 50 > ( window.innerHeight / 2 ) ) {
+					top = top - 446;
 				}
+				if ( ( window.innerWidth - ( left + 480 ) ) < 0 ) {
+					left = left - 280;
+				}
+				var img = document.createElement( 'img' );
+				img.src = link;
+				var div = document.createElement( 'div' );
+				div.classList.add( 'hoverimage' );
+				div.appendChild( img );
+				div.style.top = Math.round( top ).toString() + 'px';
+				div.style.left = Math.round( left ).toString() + 'px';
+				card.appendChild( div );
 			} );
 			card.addEventListener( 'mouseleave', function() {
+				document.querySelectorAll( '.hoverimage' ).forEach( function( card ) {
+					card.parentNode.removeChild( card );
+				} );
+			} );
+			card.addEventListener( 'wheel', function() {
 				document.querySelectorAll( '.hoverimage' ).forEach( function( card ) {
 					card.parentNode.removeChild( card );
 				} );
